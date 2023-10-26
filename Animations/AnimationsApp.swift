44b940ef1f8469d737364a50fd08bba3a7f6abed
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-25 21:04:17 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Adriano Valumin on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
