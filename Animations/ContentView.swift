3699fe1ae3245cd17a19173c26c94c34349b51bd
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-27 19:47:10 -0300

    Part 2

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,56 +7,102 @@
 
 import SwiftUI
 
+struct CornerRotateMofidier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateMofidier(amount: -90, anchor: .topLeading), identity: CornerRotateMofidier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+
     @State private var animationAmount = 1.0
-    @State private var animationAmount2 = 1.0
-    @State private var animationAmount3 = 0.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        VStack {
+        ZStack {
+            LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+
             VStack {
-                Stepper("Scale amount: \(animationAmount.formatted())", value: $animationAmount2.animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true)), in: 1 ... 10)
-                    .padding(50)
-            }
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(.blue)
+                            .frame(width: 200, height: 200)
 
-            Spacer()
-
-            Button("Hey") {
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    animationAmount3 += 360
+                        if enabled {
+                            Rectangle()
+                                .fill(.red)
+                                .frame(width: 200, height: 200)
+                                .transition(.pivot)
+                        }
+                    }
+                    .onTapGesture {
+                        withAnimation {
+                            enabled.toggle()
+                        }
+                    }
                 }
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .rotation3DEffect(
-                .degrees(animationAmount3),
-                axis: (x: -1.0, y: 1.0, z: 0.0)
-            )
 
-            Spacer()
+                VStack {
+                    Button("Tap me") {
+                        withAnimation {
+                            enabled.toggle()
+                        }
+                    }
+                    if enabled {
+                        Rectangle()
+                            .fill(.red)
+                            .frame(width: 200, height: 200)
+                            .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    }
+                }
 
-            Button("Tap me") {
-                animationAmount += 1
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount2)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(0.2)
-                    .animation(
-                        .easeInOut(duration: 2)
-                            .repeatForever(autoreverses: true),
-                        value: animationAmount)
-            )
-            .onAppear {
-                animationAmount = 2
+                ZStack {
+                    Button("") {
+                        enabled.toggle()
+                    }
+                    .frame(width: 300, height: 200)
+                    .background(enabled ? .blue : .red)
+                    .foregroundStyle(.white)
+                    .animation(.default, value: enabled) // animates only the background color transition
+                    .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+                    .animation(.spring(duration: 1, bounce: 0.6), value: enabled) // animates only the clipShape
+                    .border(Color.black, width: enabled ? 0 : 3)
+                    .animation(.easeIn, value: enabled) // animates only the border
+
+                    HStack(spacing: 0) {
+                        ForEach(0 ..< letters.count, id: \.self) { num in
+                            Text(String(letters[num]))
+                                .padding(5)
+                                .font(.title)
+
+                                .clipShape(.rect(cornerRadius: 1))
+                                .offset(dragAmount)
+                                .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                        }
+                    }
+                    .background(enabled ? .red : .blue).gesture(
+                        DragGesture()
+                            .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in
+                                dragAmount = .zero
+                                enabled.toggle()
+                            }
+                    )
+                }
             }
         }
     }
